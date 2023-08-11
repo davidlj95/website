9325f133735efe7e35b49b4039101eb6b3c0adc0
David LJ <mail@davidlj95.com>
2023-08-11 17:49:06 +0200

    feat(home): extract consts to component
    (cherry picked from commit d79f52265501d8704bd6b7fa8be358ca63e01e7d)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = '@davidlj95/website';
+  realname = 'David LJ';
+  nickname = 'davidlj95';
+  phone = '+34 644 449 360';
+  email = 'mail@davidlj95.com';
+  github_url =  'https://github.com/davidlj95';
+  stackoverflow_url = 'https://stackoverflow.com/users/3263250/davidlj95';
+  linkedin_url = 'https://www.linkedin.com/in/davidlj95';
+  keybase_url = 'https://keybase.io/davidlj95';
+  twitter_url = 'https://twitter.com.com/davidlj95';
+  instagram_url = 'https://www.instagram.com/davidlj95';
+  facebook_url = 'https://www.facebook.com/davidlj95';
+  resume_url = 'https://resume.davidlj95.com';
 }
