26038fbe38972e2a5340d6bb1cb261b3969d4f1e
David LJ <mail@davidlj95.com>
2023-08-15 23:33:41 +0200

    fix(home): enable JS-only components only on client
    (cherry picked from commit 81c0b68e4f3e6a65efaeebe8b63421fef9b07d41)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { DOCUMENT } from "@angular/common";
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -21,19 +21,23 @@ export class AppComponent implements OnInit {
   resume_url = 'https://resume.davidlj95.com';
   noScriptCssClass = 'hideIfNoScript';
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: string,
+  ) {
   }
 
   ngOnInit(): void {
-    this.displayJavascriptPoweredElements();
+    if (isPlatformBrowser(this.platformId)) {
+      this.displayJavascriptPoweredElements();
+    }
   }
 
   private displayJavascriptPoweredElements() {
-    // TODO: set for client only
-    //const scriptPoweredElements = this.document.querySelectorAll(`.${this.noScriptCssClass}`);
-    //scriptPoweredElements.forEach((element) => {
-    //  element.classList.remove(this.noScriptCssClass);
-    //});
+    const scriptPoweredElements = this.document.querySelectorAll(`.${this.noScriptCssClass}`);
+    scriptPoweredElements.forEach((element) => {
+      element.classList.remove(this.noScriptCssClass);
+    });
   }
 
   toggleTheme() {
