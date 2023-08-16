f0c2337c5a2359231be6459d09919de763f51d4a
David LJ <mail@davidlj95.com>
2023-08-16 22:38:09 +0200

    feat(social): add svg icons
    (cherry picked from commit fcdbd1a2599a467fd53b2c84098d42327ddce91b)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { NoScriptComponent } from './no-script/no-script.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { SocialNetworksComponent } from './social-networks/social-networks.component';
 import { NavigationTabsComponent } from './navigation-tabs/navigation-tabs.component';
-import {WindowComponent} from './window/window.component';
+import { WindowComponent } from './window/window.component';
 
 @NgModule({
   declarations: [
