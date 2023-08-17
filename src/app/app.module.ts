f1f09b1011ce3581571a38d937fb62f3c11cecbb
David LJ <mail@davidlj95.com>
2023-08-17 18:53:25 +0200

    feat(seo): use @ngaox/seo to introduce SEO metadata
    (cherry picked from commit 6241ddb7eb3d82f3d20cd571da259dece9db34e5)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { SocialNetworksComponent } from './social-networks/social-networks.component';
 import { NavigationTabsComponent } from './navigation-tabs/navigation-tabs.component';
 import { WindowComponent } from './window/window.component';
+import { SeoModule } from "@ngaox/seo";
+
+const nickname = 'davidlj95';
+const realname = 'David LJ';
+const siteName = `${realname} 🔗 @${nickname}`;
 
 @NgModule({
   declarations: [
@@ -27,6 +32,32 @@ import { WindowComponent } from './window/window.component';
     BrowserModule,
     AppRoutingModule,
     NgOptimizedImage,
+    SeoModule.forRoot({
+      title: siteName,
+      keywords: `${nickname}, website, ${realname}, portfolio, cv, resume, projects, info, contact`,
+      description: '/dev/random software engineer. Connecting technology &amp; RealLife™. Padel frequent player. Get to know me better here',
+      url: `https://v2.${nickname}.com/`,
+      type: 'website',
+      image: {
+        url: `https://${nickname}.com/assets/img/og.jpg`,
+        alt: `A portrait of ${realname}. Slightly smiling and wearing geek'ish glasses`,
+        width: 875,
+        height: 875,
+        // mimeType: "Not setting it. So you can change the image without needing to worry about updating this l8r",
+      },
+      twitter: {
+        card: 'summary',
+        creator: `@${nickname}`,
+        site: `@${nickname}`,
+      },
+      siteName: siteName,
+      extra: [
+        {name: 'author', content: nickname,},
+        {property: 'og:locale', content: 'en',},
+        {property: 'fb:admins', content: nickname,},
+        {name: 'facebook-domain-verification', content: '1299426610587748'}
+      ]
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
