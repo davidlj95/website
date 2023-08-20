a842d6c555ee068c06b0dad38acb3738f0ee80ee
David LJ <mail@davidlj95.com>
2023-08-20 06:27:22 +0200

    feat(security): add security.txt
    add template
    add generation script from template (signs it)
    add script to tsconfig
    add first security.txt
    
    (cherry picked from commit c87de86ffb30ba5aa2c79f9049d24b49ed118f4c)

---
+++ scripts/generate-security-txt.ts
@@ -0,0 +1,31 @@
+import * as child_process from 'child_process';
+import * as fs from 'fs/promises';
+import { Liquid } from 'liquidjs';
+import * as path from 'path';
+import * as util from 'util';
+import { generateTemplatedFile, LIQUID_EXTENSION } from './generate-templated-files';
+import { getRepositoryRootDir, isMain, Log, SECURITY_TXT_REL_PATH } from './utils';
+
+async function generateSecurityTxt() {
+  Log.info('Rendering security.txt from template')
+  const securityTxtFile = path.resolve(getRepositoryRootDir(), 'src', SECURITY_TXT_REL_PATH)
+  const engine = new Liquid({
+    dateFormat: '%Y-%m-%d %H:%M:%S+00:00',
+    timezoneOffset: 0,
+  })
+  await generateTemplatedFile(`${securityTxtFile}${LIQUID_EXTENSION}`, {engine: engine})
+
+  Log.info('Signing file with GPG')
+  const exec = util.promisify(child_process.exec)
+  const result = await exec(`gpg --output - --clear-sign ${securityTxtFile}`)
+  Log.ok('Signed')
+
+  Log.info('Writing signed file');
+  await fs.writeFile(securityTxtFile, result['stdout']);
+
+  Log.ok('Security.txt generated');
+}
+
+if (isMain(import.meta.url)) {
+  await generateSecurityTxt();
+}
