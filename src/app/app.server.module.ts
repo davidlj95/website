0dde0c32399f9a3754b808809d44b19c6d448ace
David LJ <mail@davidlj95.com>
2023-08-23 00:51:26 +0200

    fix: dummy window injection when server rendering
    (cherry picked from commit 2f4b45aa6f46f8bcad669a928ebdf35fba99f109)

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,11 +3,16 @@ import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
 
 import { AppModule } from './app.module';
+import { WINDOW } from './common/window-injection-token';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+  ],
+  providers: [
+    // No window available on server. Shouldn't be used anyway
+    {provide: WINDOW, useValue: {}}
   ],
   bootstrap: [AppComponent],
 })
