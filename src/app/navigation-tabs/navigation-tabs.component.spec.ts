1f164f56a4c298e3791ac5204a258e7b7cfed2fc
David LJ <mail@davidlj95.com>
2023-08-26 20:07:48 +0200

    test(navigation): add tests to navigation tabs
    (cherry picked from commit 2a0e9eebc2939e2dac6f4deb7a2e554f3b14cbf6)

--- src/app/navigation-tabs/navigation-tabs.component.spec.ts
+++ src/app/navigation-tabs/navigation-tabs.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { NavigationTabsComponent } from './navigation-tabs.component';
 
@@ -18,4 +19,25 @@ describe('NavigationTabsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain all navigation items with their name and link', () => {
+    const tabElements = fixture.debugElement.queryAll(By.css('li'))
+    expect(tabElements).toHaveSize(component.items.length);
+    tabElements.forEach((tabElement, index) => {
+      const tabItem = component.items[index];
+      expect(tabElement.nativeElement.textContent.trim()).withContext(`Tab ${index} text`).toEqual(tabItem.displayName);
+      const link = tabElement.query(By.css('a'));
+      expect(link.attributes['href']).withContext(`Tab ${index} link`).toEqual(tabItem.externalUrl ?? `#${tabItem.id}`);
+    });
+  });
+
+  it('should open external links in new tab', () => {
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    const externalLinks = links.filter(
+      (link) => !!link.attributes['href'] && !link.attributes['href'].startsWith('#')
+    )
+    externalLinks.forEach((externalLink, index) => {
+      expect(externalLink.attributes['target']).withContext(`External link ${index}`).toEqual('_blank');
+    });
+  })
 });
