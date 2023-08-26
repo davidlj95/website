e40551126a94ce0b0a707a97b0a8adff4e3de9b6
David LJ <mail@davidlj95.com>
2023-08-26 21:46:25 +0200

    test(contacts): ensure JSON compatible, all items & links
    (cherry picked from commit fbf92deb0afa8c212413efa5fc7707010ed2e2a8)

--- src/app/contacts/contacts.component.spec.ts
+++ src/app/contacts/contacts.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ContactsComponent } from './contacts.component';
 
@@ -18,4 +19,23 @@ describe('ContactsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render is if it was a JSON object, containing all contacts', () => {
+    const jsonText = fixture.debugElement.nativeElement.textContent;
+    const jsonTextWithoutNbsp = jsonText.replace(/\u00A0/g, '');
+    const parsedJSON = JSON.parse(jsonTextWithoutNbsp);
+    component.items.forEach((item) => {
+      expect(parsedJSON[item.key]).withContext(`Item ${item.key}`).toEqual(item.value);
+    });
+  });
+
+  it('should link all values using the URI prefix', () => {
+    const valueElements = fixture.debugElement.queryAll(By.css('dd'));
+    expect(valueElements).toHaveSize(component.items.length);
+    component.items.forEach((item, index) => {
+      const valueElement = valueElements[index]
+      const anchorElement = valueElement.query(By.css('a'))
+      expect(anchorElement.attributes['href']).withContext(`Link ${item.key}`).toEqual(`${item.uriPrefix}${item.value}`)
+    })
+  })
 });
