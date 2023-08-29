9ea399757bd1668678eba66dc25c37ac169ec7eb
David LJ <mail@davidlj95.com>
2023-08-29 06:32:53 +0200

    test: enforce 80% coverage on everything
    (cherry picked from commit 5715d2a23abfe5c00adf43220c4c5a367825a2ad)

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,15 @@ module.exports = function (config) {
       reporters: [
         {type: 'html'},
         {type: 'text-summary'}
-      ]
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
