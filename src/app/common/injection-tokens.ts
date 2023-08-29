2a229487897fe370a18c44c04c977174cbef8c82
David LJ <mail@davidlj95.com>
2023-08-29 06:33:07 +0200

    chore: add comments to ignore specific utils for coverage
    (cherry picked from commit 9dda8cd775868c24902cb2cc0771da1a00f7b895)

--- src/app/common/injection-tokens.ts
+++ src/app/common/injection-tokens.ts
@@ -2,12 +2,14 @@ import { InjectionToken } from '@angular/core';
 import { environment, Environment } from '../../environments';
 import { METADATA as METADATA_OBJECT, Metadata } from '../metadata';
 
+/* istanbul ignore next */
 export const ENVIRONMENT = new InjectionToken<Environment>('Environment config', {
   factory: () => environment,
 });
+/* istanbul ignore next */
 export const METADATA = new InjectionToken<Metadata>('Metadata object', {
   factory: () => METADATA_OBJECT,
 });
 export const WINDOW = new InjectionToken<Window>('Global window object', {
-  factory: () => window
+  factory: () => window,
 });
--- src/utils.ts
+++ src/utils.ts
@@ -4,5 +4,6 @@
  * @see https://stackoverflow.com/questions/58278652/generic-enum-type-guard#comment130450749_58278753
  * @param e Enum to create a type guard for
  */
+/* istanbul ignore next */
 export const isSomeEnum = <T extends { [s: string]: unknown }>(e: T) => (token: unknown): token is T[keyof T] =>
   Object.values(e).includes(token as T[keyof T]);
