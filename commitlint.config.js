5c30e9acf1266a60c488832a41677a7563d27040
David LJ <mail@davidlj95.com>
2023-09-05 23:49:02 +0200

    ci(dependabot): remove patches added for it
    (cherry picked from commit 9f38afea03fea53ae0359182ea12bb438fc75d3d)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  // Let Dependabot write its commits, which are semantic, but too long
-  ignores: [(commitMessage) => commitMessage.match(/^build\(deps(?:-dev)?\): bump/)]
 }
