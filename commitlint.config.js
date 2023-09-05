3add573e988c9a158f65c4f753dcdb3e52372e1e
David LJ <mail@davidlj95.com>
2023-09-05 22:39:06 +0200

    build(commitlint): allow dependabot bump commit messages
    (cherry picked from commit a04902af708821cbab7a1e87580dd835b6862f1e)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  // Let Dependabot write its commits, which are semantic, but too long
+  ignores: [(commitMessage) => commitMessage.match(/^build\(deps(?:-dev)?\): bump/)]
 }
