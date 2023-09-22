8b77d345b93ce9ef373149048ad2043c9044773c
David LJ <mail@davidlj95.com>
2023-09-22 05:45:54 +0200

    feat(profile): make easter egg accessible
    displays "huh" photo version when passing by the element again
    says a message about passing by again instead

--- src/app/about/profile-picture/profile-picture.component.spec.ts
+++ src/app/about/profile-picture/profile-picture.component.spec.ts
@@ -1,3 +1,4 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
@@ -9,31 +10,70 @@ describe('ProfilePictureComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProfilePictureComponent]
+      declarations: [ProfilePictureComponent],
     });
     fixture = TestBed.createComponent(ProfilePictureComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  const PROFILE_PIC_MAIN_SELECTOR = By.css('img.main')
+  const PROFILE_PIC_HUH_SELECTOR = By.css('img.huh')
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should display profile picture', () => {
-    const profilePic = fixture.debugElement.query(By.css('img.main'));
+    const profilePic = fixture.debugElement.query(PROFILE_PIC_MAIN_SELECTOR);
     expect(profilePic).toBeTruthy();
     expect(profilePic.attributes['ngSrc']).toBeDefined();
     expect(profilePic.attributes['ngSrc']).toContain('profile.png');
   })
 
-  it('should display "huh" profile picture hidden', () => {
-    const huhProfilePic = fixture.debugElement.query(By.css('img.huh'));
+  it('should contain "huh" profile picture, despite hidden', () => {
+    const huhProfilePic = fixture.debugElement.query(PROFILE_PIC_HUH_SELECTOR);
     expect(huhProfilePic).toBeTruthy();
     expect(huhProfilePic.attributes['ngSrc']).toBeDefined();
     expect(huhProfilePic.attributes['ngSrc']).toContain('profile_huh.png');
     const styles = getComputedStyle(huhProfilePic.nativeElement);
     expect(styles).toBeTruthy();
     expect(styles.opacity).toEqual('0');
+    expect(styles.visibility).toEqual('hidden');
+  })
+
+  describe('accessible easter egg', () => {
+    describe('initially', () => {
+      it('component should not have has been focused attribute', () => {
+        expect(fixture.debugElement.attributes[ProfilePictureComponent.HAS_BEEN_FOCUSED_ATTR]).toBeUndefined()
+      })
+      // We don't receive focus events if including whole component in tab sequence
+      it('component as a whole should not be in tab sequence', () => {
+        expect(fixture.debugElement.attributes['tabindex']).toBe('-1')
+      })
+      // So we include profile picture instead
+      it('main profile pic should be in tab sequence', () => {
+        const profilePic = fixture.debugElement.query(PROFILE_PIC_MAIN_SELECTOR);
+        expect(profilePic.attributes['tabindex']).toBe('0')
+      })
+    })
+    describe('after profile picture has received focus at least one', () => {
+      let profilePicEl: DebugElement
+
+      beforeEach(() => {
+        profilePicEl = fixture.debugElement.query(PROFILE_PIC_MAIN_SELECTOR)
+        profilePicEl.triggerEventHandler('focus')
+        fixture.detectChanges()
+      })
+
+      it('component should have has been focused attribute', () => {
+        expect(fixture.debugElement.attributes[ProfilePictureComponent.HAS_BEEN_FOCUSED_ATTR]).toBe(true.toString())
+      })
+      it('component should be included in tab sequence', () => {
+        expect(fixture.debugElement.attributes['tabindex']).toBe('0')
+      })
+      it('profile pic should no longer be in tab sequence', () => {
+        expect(profilePicEl.attributes['tabindex']).toBe('-1')
+      })
+    })
   })
 });
--- src/app/about/profile-picture/profile-picture.component.ts
+++ src/app/about/profile-picture/profile-picture.component.ts
@@ -1,17 +1,34 @@
-import { Component, Inject } from '@angular/core';
+import { Component, HostBinding, Inject } from '@angular/core';
 import { METADATA } from '../../common/injection-tokens';
 import { Metadata } from '../../metadata';
 
 @Component({
   selector: 'app-profile-picture',
   templateUrl: './profile-picture.component.html',
-  styleUrls: ['./profile-picture.component.scss']
+  styleUrls: ['./profile-picture.component.scss'],
 })
 export class ProfilePictureComponent {
   protected realName: string = this.metadata.realName;
+  protected _hasBeenFocused= false;
+
+  public static HAS_BEEN_FOCUSED_ATTR = 'data-has-been-focused'
+
+  @HostBinding(`attr.${ProfilePictureComponent.HAS_BEEN_FOCUSED_ATTR}`) public get hasBeenFocused() {
+    return this._hasBeenFocused ? true : undefined
+  }
+
+  @HostBinding('attr.tabindex') public get tabIndex() {
+    return this.hasBeenFocused ? 0 : -1
+  }
+
+  @HostBinding('attr.aria-label') public ariaLabel  = 'Profile picture'
 
   constructor(
     @Inject(METADATA) private metadata: Metadata,
   ) {
+  }
+
+  onFocus() {
+    this._hasBeenFocused = true
   }
 }
