63a1efbe9e42905caf68daec23efa03a82dbfe91
David LJ <mail@davidlj95.com>
2023-09-29 09:46:45 +0200

    feat(noscript): extract from main tag
    and several other improvements

--- src/app/no-script/no-script.component.ts
+++ src/app/no-script/no-script.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core'
+import { Component } from '@angular/core'
 import { Warning } from '../material-symbols'
 
 @Component({
   selector: 'app-no-script',
   templateUrl: './no-script.component.html',
   styleUrls: ['./no-script.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class NoScriptComponent {
   protected MaterialSymbol = {
