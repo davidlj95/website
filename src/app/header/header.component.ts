92050889df0542b45d47d01f2d6998fd973a7c30
David LJ <mail@davidlj95.com>
2023-09-29 09:46:45 +0200

    feat(noscript): extract from main tag
    and several other improvements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core'
+import { Component } from '@angular/core'
 import { DarkTheme, LightTheme } from '../material-symbols'
 import { ColorSchemeService } from './color-scheme.service'
 
@@ -8,8 +8,6 @@ import { ColorSchemeService } from './color-scheme.service'
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  @HostBinding('attr.role') ariaRole = 'toolbar'
-
   protected MaterialSymbol = {
     DarkTheme,
     LightTheme,
