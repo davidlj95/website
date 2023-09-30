1b3a3116e6150c5f8e39a85cfc97468946325493
David LJ <mail@davidlj95.com>
2023-09-30 12:13:04 +0200

    feat(not-found): configure not found routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AboutComponent } from './about/about.component'
+import { NotFoundComponent } from './not-found/not-found.component'
 
 const routes: Routes = [
   { path: '', component: AboutComponent, pathMatch: 'full' },
-  { path: '**', redirectTo: '' },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent },
 ]
 
 @NgModule({
