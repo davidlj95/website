3d3a87c1c95914a7d964393eaaf56fa94167830e
David LJ <mail@davidlj95.com>
2023-09-30 18:23:09 +0200

    feat(not-found): configure metadata for url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AboutComponent } from './about/about.component'
 import { NotFoundComponent } from './not-found/not-found.component'
+import { IPageSeoData } from '@ngaox/seo'
+import { METADATA } from './metadata'
 
+const notFoundData: { NgaoxSeo: IPageSeoData } = {
+  NgaoxSeo: {
+    title: `${METADATA.siteName} | Not Found`,
+    description: 'Page could not be found',
+    keywords: undefined,
+    url: undefined,
+    image: undefined,
+  },
+}
 const routes: Routes = [
   { path: '', component: AboutComponent, pathMatch: 'full' },
-  { path: '404', component: NotFoundComponent },
-  { path: '**', component: NotFoundComponent },
+  { path: '404', component: NotFoundComponent, data: notFoundData },
+  { path: '**', component: NotFoundComponent, data: notFoundData },
 ]
 
 @NgModule({
