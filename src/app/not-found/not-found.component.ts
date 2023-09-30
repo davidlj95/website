0dae903053cf84bcade7f686c164ebf895f03e29
David LJ <mail@davidlj95.com>
2023-09-30 13:36:45 +0200

    feat(not-found): add tip box
    plus some padding and text align center

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core'
+import { Lightbulb } from '../material-symbols'
+import { MATERIAL_SYMBOLS_CLASS } from '../common/material-symbols'
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  protected readonly MATERIAL_SYMBOLS_CLASS = MATERIAL_SYMBOLS_CLASS
+  protected readonly MaterialSymbol = {
+    Lightbulb,
+  }
+}
