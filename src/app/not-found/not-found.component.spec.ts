acfdd9af6cf4d9801c4be5959199c46f2100ece6
David LJ <mail@davidlj95.com>
2023-10-01 10:25:21 +0200

    feat(not-found): add wayback machine current url link

--- src/app/not-found/not-found.component.spec.ts
+++ src/app/not-found/not-found.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { NotFoundComponent } from './not-found.component'
+import {
+  NotFoundComponent,
+  WAYBACK_MACHINE_URL_PREFIX,
+} from './not-found.component'
+import { Environment } from '../../environments'
+import { Router } from '@angular/router'
+import { MockProvider } from 'ng-mocks'
+import { ENVIRONMENT } from '../common/injection-tokens'
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent
   let fixture: ComponentFixture<NotFoundComponent>
+  const fakeEnvUrlNoTrailingSlash: string = 'https://example.com'
+  const fakeEnv: Pick<Environment, 'canonicalUrl'> = {
+    canonicalUrl: new URL(fakeEnvUrlNoTrailingSlash),
+  }
+  const fakeRouter: Pick<Router, 'url'> = {
+    url: '/foo',
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [NotFoundComponent],
+      providers: [
+        MockProvider(ENVIRONMENT, fakeEnv),
+        MockProvider(Router, fakeRouter),
+      ],
     })
     fixture = TestBed.createComponent(NotFoundComponent)
     component = fixture.componentInstance
@@ -17,5 +35,17 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  describe('#currentUrlInWaybackMachine', () => {
+    it('should be the Wayback Machine URL prefix plus the current URL', () => {
+      expect(component.currentUrlInWaybackMachine).toEqual(
+        new URL(
+          WAYBACK_MACHINE_URL_PREFIX.toString() +
+            fakeEnvUrlNoTrailingSlash +
+            fakeRouter.url,
+        ),
+      )
+    })
   })
 })
--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, Inject } from '@angular/core'
 import { Lightbulb } from '../material-symbols'
 import { MATERIAL_SYMBOLS_CLASS } from '../common/material-symbols'
+import { Router } from '@angular/router'
+import { Environment } from '../../environments'
+import { ENVIRONMENT } from '../common/injection-tokens'
 
 @Component({
   selector: 'app-not-found',
@@ -8,8 +11,20 @@ import { MATERIAL_SYMBOLS_CLASS } from '../common/material-symbols'
   styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent {
+  public readonly currentUrlInWaybackMachine: URL
   protected readonly MATERIAL_SYMBOLS_CLASS = MATERIAL_SYMBOLS_CLASS
   protected readonly MaterialSymbol = {
     Lightbulb,
   }
+
+  constructor(@Inject(ENVIRONMENT) environment: Environment, router: Router) {
+    const currentUrl = new URL(router.url, environment.canonicalUrl)
+    this.currentUrlInWaybackMachine = new URL(
+      `${WAYBACK_MACHINE_URL_PREFIX}${currentUrl}`,
+    )
+  }
 }
+
+export const WAYBACK_MACHINE_URL_PREFIX = new URL(
+  'https://web.archive.org/web/*/',
+)
