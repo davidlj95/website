6cb42d66115ee93c5b70642798ebc3d48718ca0a
David LJ <mail@davidlj95.com>
2023-10-12 00:13:12 +0200

    refactor(experience): remove "professional" prefix

--- src/app/about/experience/experience.component.ts
+++ src/app/about/experience/experience.component.ts
@@ -3,11 +3,11 @@ import { ExperienceItem } from './experience-item/experience-item'
 import { ExperienceItemsService } from './experience-items.service'
 
 @Component({
-  selector: 'app-professional-experience',
-  templateUrl: './professional-experience.component.html',
-  styleUrls: ['./professional-experience.component.scss'],
+  selector: 'app-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.scss'],
 })
-export class ProfessionalExperienceComponent {
+export class ExperienceComponent {
   protected items: ReadonlyArray<ExperienceItem>
 
   constructor(experienceItemsService: ExperienceItemsService) {
