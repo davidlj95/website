d49a23996d006c80513026aec8dfc5ac790a2098
David LJ <mail@davidlj95.com>
2024-03-13 13:29:53 +0100

    chore: remove unneeded module imports from config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,14 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { ApplicationConfig } from '@angular/core'
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router'
-import { NgOptimizedImage } from '@angular/common'
 import { APP_METADATA_PROVIDERS } from './app.metadata-imports'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { routes } from './app.routes'
 import { provideAnimations } from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(BrowserModule, NgOptimizedImage, FontAwesomeModule),
     provideRouter(
       routes,
       // 👇 Needed for SSR
