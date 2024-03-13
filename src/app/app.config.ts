4dd4f6f022613e1dd9eb27199e6d475f33907adc
David LJ <mail@davidlj95.com>
2024-03-13 13:15:01 +0100

    refactor: move app config to its own file

---
+++ src/app/app.config.ts
@@ -0,0 +1,31 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import {
+  provideRouter,
+  RouterModule,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router'
+import { NgOptimizedImage } from '@angular/common'
+import { APP_MODULE_METADATA_IMPORTS } from './app.module'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { routes } from './app.routes'
+import { provideAnimations } from '@angular/platform-browser/animations'
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      // 👇 Temporary until app component is standalone
+      RouterModule,
+      NgOptimizedImage,
+      ...APP_MODULE_METADATA_IMPORTS,
+      FontAwesomeModule,
+    ),
+    provideRouter(
+      routes,
+      // 👇 Needed for SSR
+      withEnabledBlockingInitialNavigation(),
+    ),
+    provideAnimations(),
+  ],
+}
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-
-import { APP_MODULE_METADATA_IMPORTS } from './app/app.module'
-import { importProvidersFrom } from '@angular/core'
 import { AppComponent } from './app/app.component'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { NgxMetaTwitterCardModule } from '@davidlj95/ngx-meta/twitter-card'
-import { NgxMetaOpenGraphModule } from '@davidlj95/ngx-meta/open-graph'
-import { NgxMetaStandardModule } from '@davidlj95/ngx-meta/standard'
-import { NgxMetaRoutingModule } from '@davidlj95/ngx-meta/routing'
-import { METADATA_DEFAULTS } from './app/app.metadata-defaults'
-import { NgxMetaCoreModule } from '@davidlj95/ngx-meta/core'
-import { NgOptimizedImage } from '@angular/common'
-import { provideAnimations } from '@angular/platform-browser/animations'
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser'
-import { routes } from './app/app.routes'
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-  RouterModule,
-} from '@angular/router'
+import { appConfig } from './app/app.config'
+import { bootstrapApplication } from '@angular/platform-browser'
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      BrowserModule,
-      // 👇 Temporary until app component is standalone
-      RouterModule,
-      NgOptimizedImage,
-      ...APP_MODULE_METADATA_IMPORTS,
-      FontAwesomeModule,
-    ),
-    provideRouter(
-      routes,
-      // 👇 Needed for SSR
-      withEnabledBlockingInitialNavigation(),
-    ),
-    provideAnimations(),
-  ],
-}).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
