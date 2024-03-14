6a488a5a673efbaa73b7396f64eb59e59ac1a792
David LJ <mail@davidlj95.com>
2024-03-14 16:27:59 +0100

    refactor: use consts for noscript css helpers (#295)

---
+++ src/app/common/no-script.ts
@@ -0,0 +1,2 @@
+export const DISPLAY_BLOCK_IF_NO_SCRIPT_CLASS = 'displayBlockIfNoScript'
+export const DISPLAY_NONE_IF_NO_SCRIPT_CLASS = 'displayNoneIfNoScript'
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core'
 import { DarkTheme, LightTheme } from '../material-symbols'
 import { ColorSchemeService } from './color-scheme.service'
 import { MaterialSymbolDirective } from '../common/material-symbol.directive'
+import { DISPLAY_NONE_IF_NO_SCRIPT_CLASS } from '../common/no-script'
+import { NgClass } from '@angular/common'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [MaterialSymbolDirective],
+  imports: [MaterialSymbolDirective, NgClass],
 })
 export class HeaderComponent {
   protected readonly MaterialSymbol = {
@@ -17,4 +19,7 @@ export class HeaderComponent {
   }
 
   constructor(protected readonly colorSchemeService: ColorSchemeService) {}
+
+  protected readonly DISPLAY_NONE_IF_NO_SCRIPT_CLASS =
+    DISPLAY_NONE_IF_NO_SCRIPT_CLASS
 }
