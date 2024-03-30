83856b3203551866d746706bacdd9c7793d27906
David LJ <mail@davidlj95.com>
2024-03-30 19:54:22 +0100

    chore: temporarily remove animations for child content

--- src/app/resume-page/experience-section/experience-item/experience-item-highlights/experience-item-highlights.component.ts
+++ src/app/resume-page/experience-section/experience-item/experience-item-highlights/experience-item-highlights.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  HostBinding,
-  HostListener,
-  Input,
-} from '@angular/core'
-import { slideDownOnEnterAndSlideUpOnLeave } from '@common/animations'
+import { Component, Input } from '@angular/core'
 import { NgFor } from '@angular/common'
 
 @Component({
@@ -16,17 +9,9 @@ import { NgFor } from '@angular/common'
     </li>
   </ul>`,
   styleUrls: ['./experience-item-highlights.component.scss'],
-  animations: [slideDownOnEnterAndSlideUpOnLeave('enterAndLeave')],
   standalone: true,
   imports: [NgFor],
 })
 export class ExperienceItemHighlightsComponent {
   @Input({ required: true }) public highlights!: readonly string[]
-
-  @HostBinding('@enterAndLeave') public enterAndLeaveAnimation = true
-  public enterAndLeaveAnimationDone = new EventEmitter<void>()
-  @HostListener('@enterAndLeave.done')
-  protected onEnterAndLeaveAnimationDone() {
-    this.enterAndLeaveAnimationDone.emit()
-  }
 }
--- src/app/resume-page/experience-section/experience-item/experience-item-summary/experience-item-summary.component.ts
+++ src/app/resume-page/experience-section/experience-item/experience-item-summary/experience-item-summary.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  HostBinding,
-  HostListener,
-  Input,
-} from '@angular/core'
-import { slideDownOnEnterAndSlideUpOnLeave } from '@common/animations'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-experience-item-summary',
@@ -17,17 +10,9 @@ import { slideDownOnEnterAndSlideUpOnLeave } from '@common/animations'
       }
     `,
   ],
-  animations: [slideDownOnEnterAndSlideUpOnLeave('enterAndLeave')],
   standalone: true,
 })
 export class ExperienceItemSummaryComponent {
   @Input({ required: true })
   public summary!: string
-
-  @HostBinding('@enterAndLeave') public readonly enterOrLeaveAnimation = true
-  public enterAndLeaveAnimationDone = new EventEmitter<void>()
-  @HostListener('@enterAndLeave.done')
-  protected onEnterAndLeaveAnimationDone() {
-    this.enterAndLeaveAnimationDone.emit()
-  }
 }
