2f55c591d6682b021342c2644a60474a93148ad1
David LJ <mail@davidlj95.com>
2024-04-24 17:41:52 +0200

    refactor: attribute component to generate IDs (#409)

--- src/app/resume-page/attribute/attribute.component.spec.ts
+++ src/app/resume-page/attribute/attribute.component.spec.ts
@@ -1,32 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-
-import { AttributeComponent } from './attribute.component'
+import { ATTRIBUTE_ID_PREFIX, AttributeComponent } from './attribute.component'
 import { MATERIAL_SYMBOLS_SELECTOR } from '@/test/helpers/material-symbols'
 import { By } from '@angular/platform-browser'
 import { shouldProjectContent } from '@/test/helpers/component-testers'
+import { componentTestSetup } from '@/test/helpers/component-test-setup'
 
 describe('AttributeComponent', () => {
   const symbol = 'some symbol'
-  const id = 'some-attribute-id'
 
-  function makeSut(): [
-    ComponentFixture<AttributeComponent>,
-    AttributeComponent,
-  ] {
-    TestBed.configureTestingModule({})
-    const fixture = TestBed.createComponent(AttributeComponent)
-    const component = fixture.componentInstance
-
+  function makeSut() {
+    const [fixture, component] = componentTestSetup(AttributeComponent)
     component.symbol = symbol
-    component.id = id
-
     fixture.detectChanges()
-
-    return [fixture, component]
+    return [fixture, component] as const
   }
 
   it('should create', () => {
-    const [component] = makeSut()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, component] = makeSut()
 
     expect(component).toBeTruthy()
   })
@@ -42,16 +32,16 @@ describe('AttributeComponent', () => {
   it('should be ARIA accessible: icon is described by tooltip', () => {
     const [fixture] = makeSut()
 
-    const tooltipId = `${id}-tooltip`
-    const iconElement = fixture.debugElement.query(MATERIAL_SYMBOLS_SELECTOR)
-    expect(iconElement).toBeTruthy()
-    expect(iconElement.attributes['aria-describedby']).toEqual(tooltipId)
-
     const tooltipElement = fixture.debugElement.query(
       By.css("[role='tooltip']"),
     )
     expect(tooltipElement).toBeTruthy()
-    expect(tooltipElement.attributes['id']).toEqual(tooltipId)
+    const tooltipId = tooltipElement.attributes['id']
+    expect(tooltipId).toMatch(`^${ATTRIBUTE_ID_PREFIX}`)
+
+    const iconElement = fixture.debugElement.query(MATERIAL_SYMBOLS_SELECTOR)
+    expect(iconElement).toBeTruthy()
+    expect(iconElement.attributes['aria-describedby']).toEqual(tooltipId)
   })
 
   it('should be ARIA accessible: icon is included in tab sequence', () => {
