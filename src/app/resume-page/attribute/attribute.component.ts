6c567e9d639978bdd5cef5861c3265ba00e984a0
David LJ <mail@davidlj95.com>
2024-04-24 17:31:43 +0200

    refactor: attribute component to generate IDs

--- src/app/resume-page/attribute/attribute.component.ts
+++ src/app/resume-page/attribute/attribute.component.ts
@@ -1,6 +1,14 @@
 import { Component, Input } from '@angular/core'
 import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
 
+/**
+ * Pattern for unique id generation
+ * https://github.com/angular/components/blob/17.3.0/src/cdk/a11y/aria-describer/aria-describer.ts#L47-L48
+ * @visibleForTesting
+ */
+export const ATTRIBUTE_ID_PREFIX = `app-attr-`
+let nextId = 0
+
 @Component({
   selector: 'app-attribute',
   templateUrl: './attribute.component.html',
@@ -11,8 +19,5 @@ import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
 export class AttributeComponent {
   @Input({ required: true }) symbol!: string
   @Input({ required: true }) id!: string
-
-  public get tooltipId() {
-    return `${this.id}-tooltip`
-  }
+  protected _id: string = `${ATTRIBUTE_ID_PREFIX}${nextId++}`
 }
