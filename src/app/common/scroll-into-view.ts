32c698a33957287d18832cc3fac8f8d30e9a6670
David LJ <mail@davidlj95.com>
2024-04-25 20:31:34 +0200

    docs: comment about scroll into view testing

--- src/app/common/scroll-into-view.ts
+++ src/app/common/scroll-into-view.ts
@@ -6,8 +6,17 @@ import { noop } from 'rxjs'
 const scrollIntoView: ScrollIntoView = (element: HTMLElement) =>
   element.scrollIntoView && element.scrollIntoView({ block: 'nearest' })
 export type ScrollIntoView = (element: HTMLElement) => void
+
 /**
  * @visibleForTesting
+ *
+ * Otherwise, don't know how to mock the {@link PlatformService} due to `providedIn: 'platform'`
+ *
+ * Given `TestBed` configures things after platform is already bootstrapped.
+ * At that point, the token is already there with the proper {@link PlatformService}
+ *
+ * As workaround, creating another provider for it when testing using the same factory.
+ * That's why it's exported
  */
 export const _SCROLL_INTO_VIEW_FACTORY: () => ScrollIntoView = () =>
   inject(PLATFORM_SERVICE).isBrowser ? scrollIntoView : noop
