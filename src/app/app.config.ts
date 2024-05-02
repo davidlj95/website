265c162073e05a37881fadada3c8e5f59265b4b4
David LJ <mail@davidlj95.com>
2024-05-02 19:42:53 +0200

    perf: enable client hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,14 @@
 import { ApplicationConfig } from '@angular/core'
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router'
+import { provideRouter } from '@angular/router'
 import { APP_METADATA_PROVIDERS } from './app.metadata-imports'
 import { routes } from './app.routes'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideClientHydration } from '@angular/platform-browser'
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes,
-      // 👇 Needed for SSR
-      withEnabledBlockingInitialNavigation(),
-    ),
+    provideRouter(routes),
+    provideClientHydration(),
     provideAnimationsAsync(),
     ...APP_METADATA_PROVIDERS,
   ],
