aeae0c5820cdae6fef550179c90c143086e49ea2
David LJ <mail@davidlj95.com>
2024-05-13 16:15:23 +0200

    chore: remove empty scss files (#502)

--- src/app/common/simple-icon/simple-icon.component.ts
+++ src/app/common/simple-icon/simple-icon.component.ts
@@ -13,7 +13,6 @@ import { SimpleIcon } from '@/common/simple-icon/simple-icon'
   standalone: true,
   imports: [AsyncPipe],
   templateUrl: './simple-icon.component.html',
-  styleUrl: './simple-icon.component.scss',
   host: {
     '[style.fill]': '_fillColor',
   },
--- src/app/resume-page/date-range/date-range.component.ts
+++ src/app/resume-page/date-range/date-range.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input } from '@angular/core'
 import { DateRange } from './date-range'
-import { NgIf, DatePipe } from '@angular/common'
+import { DatePipe, NgIf } from '@angular/common'
 
 @Component({
   selector: 'app-date-range',
   templateUrl: './date-range.component.html',
-  styleUrls: ['./date-range.component.scss'],
   standalone: true,
   imports: [NgIf, DatePipe],
 })
