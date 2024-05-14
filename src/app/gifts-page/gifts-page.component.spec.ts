cf4939cd9e4b2a00dc32acb8efe2ada3f10d7b0a
David LJ <mail@davidlj95.com>
2024-05-14 13:29:52 +0200

    test: add some for gifts page

--- src/app/gifts-page/gifts-page.component.spec.ts
+++ src/app/gifts-page/gifts-page.component.spec.ts
@@ -1,22 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture } from '@angular/core/testing'
 
 import { GiftsPageComponent } from './gifts-page.component'
+import { componentTestSetup } from '@/test/helpers/component-test-setup'
+import { By } from '@angular/platform-browser'
 
 describe('GiftsPageComponent', () => {
   let component: GiftsPageComponent
   let fixture: ComponentFixture<GiftsPageComponent>
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GiftsPageComponent],
-    }).compileComponents()
-
-    fixture = TestBed.createComponent(GiftsPageComponent)
-    component = fixture.componentInstance
+    ;[fixture, component] = componentTestSetup(GiftsPageComponent)
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should contain link to gift preferences', () => {
+    const giftPreferencesAnchorElement = fixture.debugElement.query(
+      By.css('a[href="https://www.giftster.com/preferences/1747782/"]'),
+    )
+    expect(giftPreferencesAnchorElement).not.toBeNull()
+  })
+
+  it('should contain link to wishlist', () => {
+    const wishlistAnchorElement = fixture.debugElement.query(
+      By.css('a[href="https://www.giftster.com/list/hz6ij/"]'),
+    )
+    expect(wishlistAnchorElement).not.toBeNull()
   })
 })
