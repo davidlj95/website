c9ed265577e69b03adbd799df746fc66a05a5cee
David LJ <mail@davidlj95.com>
2024-05-16 15:50:51 +0200

    test: fix button test

--- src/app/resume-page/button/button.component.spec.ts
+++ src/app/resume-page/button/button.component.spec.ts
@@ -1,19 +1,12 @@
-import { ComponentFixture } from '@angular/core/testing'
-
 import { ButtonComponent } from './button.component'
 import { componentTestSetup } from '@/test/helpers/component-test-setup'
 import { shouldProjectContent } from '@/test/helpers/component-testers'
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent
-  let fixture: ComponentFixture<ButtonComponent>
-
-  beforeEach(async () => {
-    ;[fixture, component] = componentTestSetup(ButtonComponent)
-    fixture.detectChanges()
-  })
-
   it('should create', () => {
+    const [fixture, component] = componentTestSetup(ButtonComponent)
+    fixture.detectChanges()
+
     expect(component).toBeTruthy()
   })
 
