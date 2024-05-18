633644f929a704743cec5bf9dc2eba4b5489e811
David LJ <mail@davidlj95.com>
2024-05-18 22:53:08 +0200

    refactor: separate toolbar from header (#529)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core'
 import { LightDarkToggleComponent } from './light-dark-toggle/light-dark-toggle.component'
 import { NgClass } from '@angular/common'
+import { ToolbarComponent } from './toolbar/toolbar.component'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [NgClass, LightDarkToggleComponent],
+  imports: [NgClass, LightDarkToggleComponent, ToolbarComponent],
 })
 export class HeaderComponent {}
