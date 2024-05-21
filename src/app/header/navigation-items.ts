3a096a19ff8c8baad8f898c2db63226d1eab391e
David LJ <mail@davidlj95.com>
2024-05-21 17:21:00 +0200

    feat: add `<header>` element for a11y (#534)

---
+++ src/app/header/navigation-items.ts
@@ -0,0 +1,12 @@
+import { RESUME_PATH } from '../resume-page/resume-page.routes'
+import { CALENDAR_PATH } from '../calendar-page/calendar-page.routes'
+import { SPORTS_PATH } from '../sports-page/sports-page.routes'
+import { GIFTS_PATH } from '../gifts-page/gifts-page.routes'
+import { NavigationItem } from './navigation-tabs/navigation-tabs.component'
+
+export const NAVIGATION_ITEMS: ReadonlyArray<NavigationItem> = [
+  { displayName: 'Resume', routerLink: RESUME_PATH },
+  { displayName: 'Calendar', routerLink: CALENDAR_PATH },
+  { displayName: 'Sports', routerLink: SPORTS_PATH },
+  { displayName: 'Gifts', routerLink: GIFTS_PATH },
+].filter((_, index) => index < 2)
--- src/app/header/navigation-tabs/navigation-tabs.component.ts
+++ src/app/header/navigation-tabs/navigation-tabs.component.ts
@@ -2,12 +2,6 @@ import { Component, Input } from '@angular/core'
 import { TabComponent } from '../tab/tab.component'
 import { TabsComponent } from '../tabs/tabs.component'
 import { RouterLink, RouterLinkActive } from '@angular/router'
-import {
-  CALENDAR_PATH,
-  GIFTS_PATH,
-  RESUME_PATH,
-  SPORTS_PATH,
-} from '../../app.paths'
 
 @Component({
   selector: 'app-navigation-tabs',
@@ -17,16 +11,8 @@ import {
   styleUrl: './navigation-tabs.component.scss',
 })
 export class NavigationTabsComponent {
-  @Input() items: ReadonlyArray<NavigationItem> = NAVIGATION_ITEMS
+  @Input({ required: true }) items!: ReadonlyArray<NavigationItem>
 }
-
-export const NAVIGATION_ITEMS: ReadonlyArray<NavigationItem> = [
-  { displayName: 'Resume', routerLink: RESUME_PATH },
-  { displayName: 'Calendar', routerLink: CALENDAR_PATH },
-  { displayName: 'Sports', routerLink: SPORTS_PATH },
-  { displayName: 'Gifts', routerLink: GIFTS_PATH },
-].filter((_, index) => index < 2)
-// TODO: What to do when doesn't fit screen
 
 export interface NavigationItem {
   readonly displayName: string
