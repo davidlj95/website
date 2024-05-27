0fd3cf2ef8849a065f2003a696ff79425816bf2a
David LJ <mail@davidlj95.com>
2024-05-27 15:31:52 +0200

    refactor: add toolbar icon component

--- src/app/header/light-dark-toggle/light-dark-toggle.component.ts
+++ src/app/header/light-dark-toggle/light-dark-toggle.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
 import { DarkMode, LightMode } from '../../material-symbols'
 import { ColorSchemeService } from './color-scheme.service'
+import { ToolbarIconComponent } from '../toolbar-icon/toolbar-icon.component'
 
 @Component({
   selector: 'app-light-dark-toggle',
   standalone: true,
-  imports: [MaterialSymbolDirective],
+  imports: [MaterialSymbolDirective, ToolbarIconComponent],
   templateUrl: './light-dark-toggle.component.html',
   styleUrl: './light-dark-toggle.component.scss',
 })
