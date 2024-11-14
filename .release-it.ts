c9cd9f5035d066cbb4b475ab1e8be6cf151907c5
David LJ <mail@davidlj95.com>
2024-11-14 03:32:14 +0100

    build: use release-it to generate release data (#817)

---
+++ .release-it.ts
@@ -0,0 +1,156 @@
+import { Config } from 'release-it'
+import { BumperRecommendation, Preset } from 'conventional-recommended-bump'
+
+export const createReleaseItConfig = ({
+  gitRawCommitsOpts,
+}: ExtraConfig = {}): Config => ({
+  git: {
+    commit: false,
+    push: false,
+  },
+  // @ts-expect-error Invalid type definition. TODO: PR for this
+  npm: false,
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: {
+        name: 'conventionalcommits',
+        types: [
+          {
+            type: 'feat',
+            section: 'Features',
+          },
+          {
+            type: 'fix',
+            section: 'Bug Fixes',
+          },
+          {
+            type: 'perf',
+            section: 'Performance Improvements',
+          },
+          {
+            type: 'revert',
+            section: 'Reverts',
+          },
+          {
+            type: 'docs',
+            section: 'Documentation',
+          },
+          {
+            type: 'style',
+            section: 'Style',
+          },
+          {
+            type: 'chore',
+            section: 'Miscellaneous Chores',
+          },
+          {
+            type: 'refactor',
+            section: 'Code Refactoring',
+          },
+          {
+            type: 'test',
+            section: 'Tests',
+          },
+          {
+            type: 'build',
+            section: 'Build System',
+          },
+          {
+            type: 'ci',
+            section: 'Continuous (Integration|Deployment)',
+          },
+        ],
+      },
+      whatBump,
+      gitRawCommitsOpts,
+    },
+  },
+})
+
+/**
+ * ⚠️ Conventional Changelog version bumpers always returns at least a patch release
+ *
+ * Therefore, `release-it/conventional-changelog` behaves the same, as it's just a wrapper over it
+ * Here, the recommended version bumper is tuned so that not every commit triggers a version bump.
+ *
+ * See:
+ *
+ *  - GitHub issue about it:   https://github.com/release-it/conventional-changelog/issues/22
+ *  - Original implementation: https://github.com/conventional-changelog/conventional-changelog/blob/conventional-recommended-bump-v10.0.0/packages/conventional-changelog-conventionalcommits/src/whatBump.js
+ *
+ * @param commits
+ */
+const whatBump: Preset['whatBump'] = async (commits) => {
+  const commitsByLevel = commits.reduce(
+    (results, commit) => {
+      const addToResults = (key: number) => {
+        results[key].push(commit)
+        return results
+      }
+      // As notes are only parsed for breaking changes
+      // https://github.com/conventional-changelog/conventional-changelog/blob/conventional-recommended-bump-v10.0.0/packages/conventional-changelog-conventionalcommits/src/parser.js
+      const hasBreakingChanges = addBangNotes(commit).length > 0
+      if (hasBreakingChanges) {
+        return addToResults(RELEASE_LEVEL_MAJOR)
+      }
+      if (commit.type === 'feat' || commit.type === 'feature') {
+        return addToResults(RELEASE_LEVEL_MINOR)
+      }
+      if (commit.type === 'fix' || commit.type === 'perf') {
+        return addToResults(RELEASE_LEVEL_PATCH)
+      }
+      return results
+    },
+    [[], [], []] as [Commit[], Commit[], Commit[]],
+  )
+  const level = ifMinusOneToUndefined(
+    commitsByLevel.findIndex((commits) => commits.length > 0),
+  ) as BumperRecommendation['level']
+  const reason =
+    level === undefined
+      ? 'No commit needs to be released according to bump rules'
+      : 'These are the commits that triggered a release. The highest release level was chosen\n' +
+        commitsByLevel
+          .map(
+            (commitHeaders, level) =>
+              ` - ${RELEASE_LEVEL_NAMES[level]}: ${commitHeaders.map((header) => `"${header}"`).join(', ')}`,
+          )
+          .join('\n')
+  return { level, reason }
+}
+
+export type ExtraConfig = Partial<{
+  gitRawCommitsOpts: { from: string; to: string }
+}>
+
+// https://github.com/conventional-changelog/conventional-changelog/blob/conventional-changelog-conventionalcommits-v8.0.0/packages/conventional-changelog-conventionalcommits/src/utils.js
+function addBangNotes(commit: Commit) {
+  const breakingHeaderPattern = /^(\w*)(?:\((.*)\))?!: (.*)$/
+  const match = commit.header?.match(breakingHeaderPattern)
+  if (match && commit.notes.length === 0) {
+    const noteText = match[3] // the description of the change.
+
+    return [
+      {
+        title: 'BREAKING CHANGE',
+        text: noteText,
+      },
+    ]
+  }
+
+  return commit.notes
+}
+
+// https://stackoverflow.com/a/52331580/3263250
+type Unpacked<T> = T extends (infer U)[] ? U : T
+type Commit = Unpacked<Parameters<Preset['whatBump']>[0]>
+
+const ifMinusOneToUndefined = (n: number) => (n !== -1 ? n : undefined)
+
+// https://github.com/conventional-changelog/conventional-changelog/blob/conventional-recommended-bump-v10.0.0/packages/conventional-recommended-bump/src/bumper.ts#L24-L28
+const RELEASE_LEVEL_NAMES = ['major', 'minor', 'patch'] as const
+const [RELEASE_LEVEL_MAJOR, RELEASE_LEVEL_MINOR, RELEASE_LEVEL_PATCH] = [
+  ...RELEASE_LEVEL_NAMES.keys(),
+]
+
+export default createReleaseItConfig()
---
+++ scripts/src/generate-release-info.mts
@@ -0,0 +1,110 @@
+import release, { Config } from 'release-it'
+import { createReleaseItConfig, ExtraConfig } from '../../.release-it'
+import { getRepositoryRootDir, isMain, objectToJson } from './utils.mjs'
+import packageJson from '../../package.json' assert { type: 'json' }
+import { execSync } from 'child_process'
+import { writeFile } from 'fs/promises'
+import { join } from 'path'
+
+interface ReleaseData {
+  commitSha: string
+  current: {
+    version: string
+    changelog: string
+  }
+  next?: {
+    changelog: string
+  }
+}
+
+// TODO: PR for this
+const releaseIt = release as (config: Config) => Promise<{
+  name: string
+  changelog: string
+  latestVersion: string
+  version: string | undefined
+}>
+
+const generateReleaseInfo = async (): Promise<ReleaseData> => {
+  const dryRunReleaseResult = await dryRunReleaseIt()
+  const commitSha = execSync('git rev-parse HEAD').toString().trim()
+  if (dryRunReleaseResult.version) {
+    return {
+      commitSha,
+      current: {
+        version: dryRunReleaseResult.version,
+        changelog: dryRunReleaseResult.changelog,
+      },
+    }
+  }
+
+  const currentReleaseData = {
+    commitSha,
+    current: {
+      version: dryRunReleaseResult.latestVersion,
+      changelog: await getLastChangelog(dryRunReleaseResult.latestVersion),
+    },
+  }
+
+  const isReleasedAlready =
+    execSync('git tag --points-at HEAD').toString().trim().length > 0
+  if (isReleasedAlready) {
+    return currentReleaseData
+  }
+
+  return {
+    ...currentReleaseData,
+    next: {
+      changelog: fixUnreleasedChangelog(dryRunReleaseResult.changelog),
+    },
+  }
+}
+
+const dryRunReleaseIt = (extraConfig?: ExtraConfig) => {
+  const config = createReleaseItConfig(extraConfig)
+  return releaseIt({
+    ...config,
+    ...{
+      git: {
+        ...config.git,
+        requireCleanWorkingDir: false,
+        requireUpstream: false,
+        tag: false,
+      },
+      dryRun: true,
+    },
+  })
+}
+
+const getLastChangelog = async (lastVersion: string): Promise<string> => {
+  const previousVersion = getPreviousVersion()
+  const lastChangelog = (
+    await dryRunReleaseIt({
+      gitRawCommitsOpts: { from: previousVersion, to: `v${lastVersion}` },
+    })
+  ).changelog
+  return lastChangelog
+    .slice(lastChangelog.indexOf(`## [${lastVersion}]`))
+    .replace(`v${lastVersion}...vnull`, `${previousVersion}...v${lastVersion}`)
+}
+
+const getPreviousVersion = (): string =>
+  execSync('git describe --abbrev=0 --tags `git describe --abbrev=0 --tags`^')
+    .toString()
+    .trim()
+
+const fixUnreleasedChangelog = (changelog: string): string =>
+  changelog.replace(VERSION_PLACEHOLDER, 'Unreleased').replace('vnull', 'main')
+const VERSION_PLACEHOLDER = packageJson.version
+
+if (isMain(import.meta.url)) {
+  await writeFile(
+    join(getRepositoryRootDir(), 'src', 'release.json'),
+    objectToJson(await generateReleaseInfo()),
+  )
+  // 👇 37 handles open at this point😅 So Node.js doesn't exit
+  // Mainly due to `conventional-changelog`'s monorepo libs
+  // Maybe that's why `semantic-release` also hangs
+  // Use `why-is-node-running` for more info. In the meantime, ...
+  process.exit(0)
+}
