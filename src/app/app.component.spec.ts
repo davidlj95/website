a6640be37c5211acddf1a74630e89848094baff8
David LJ <mail@davidlj95.com>
2024-11-14 20:36:25 +0100

    chore: remove unneeded usage of deprecated router testing module (#821)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { RouterTestingModule } from '@angular/router/testing'
 import { MockComponents } from 'ng-mocks'
 import { shouldContainComponents } from '@/test/helpers/component-testers'
 import { ResumePageComponent } from './resume-page/resume-page.component'
@@ -17,7 +16,6 @@ describe('AppComponent', () => {
       imports: [
         AppComponent,
         MockComponents(NoScriptComponent, HeaderComponent, ResumePageComponent),
-        RouterTestingModule,
       ],
     })
 
