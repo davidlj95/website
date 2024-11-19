abf93c3718713164282fb6ff050cce5fbad78430
David LJ <mail@davidlj95.com>
2024-11-19 13:56:21 +0100

    test: fix Cypress component build errors

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,31 +1,11 @@
-// ***********************************************************
-// This example support/component.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
+import './cypress.d'
 import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-import { mount } from 'cypress/angular'
 
 // Cypress code coverage
 import '@cypress/code-coverage/support'
 
+// Mount alias
+import { mount } from 'cypress/angular'
 // https://youtrack.jetbrains.com/issue/AQUA-3130/
 // noinspection CypressCommandWithoutDeclaration
 Cypress.Commands.add('mount', mount)
-
-// Example use:
-// cy.mount(MyComponent)
