55a9fdd4cc1c44aa7a990d5f3cccd8f31eb1e483
David LJ <mail@davidlj95.com>
2024-11-26 16:03:06 +0100

    ci: remove no pending changes step (#848)
    * ci: remove no pending changes step
    
    * docs: update README.md with template system updates

--- .ci/ensure-no-pending-changes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-changes="$(git status --porcelain)"
-if [ -z "$changes" ]; then
-  echo "✅ No unstaged changes"
-  exit 0
-fi
-echo "❌ There are unstaged changes"
-echo "$changes"
-exit 1
