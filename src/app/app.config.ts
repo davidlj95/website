625fa94a6e28986e9f9a61e3eb3113d944f71bd5
David LJ <mail@davidlj95.com>
2024-11-26 16:35:24 +0100

    build: update app.config.ts from v19 defaults (#853)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { APP_METADATA_PROVIDERS } from './app.metadata-imports'
 import { routes } from './app.routes'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
-import { provideClientHydration } from '@angular/platform-browser'
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser'
 import { provideHttpClient } from '@angular/common/http'
 import { provideTrailingSlashUrlSerializer } from '@/common/provide-trailing-slash-url-serializer'
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
+    provideClientHydration(withEventReplay()),
     provideHttpClient(),
     provideAnimationsAsync(),
     ...APP_METADATA_PROVIDERS,
