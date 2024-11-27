24bb2402c82b232fefe6fab8a04a2d26e05d7eab
David LJ <mail@davidlj95.com>
2024-11-27 19:01:23 +0100

    refactor: do not use host decorators for chip component (#873)

--- src/app/resume-page/chip/chip.component.ts
+++ src/app/resume-page/chip/chip.component.ts
@@ -1,18 +1,19 @@
-import {
-  Component,
-  EventEmitter,
-  HostBinding,
-  HostListener,
-  input,
-  Output,
-} from '@angular/core'
+import { Component, EventEmitter, input, Output } from '@angular/core'
 
 @Component({
   selector: 'app-chip',
   template: '<ng-content></ng-content>',
   styleUrls: ['./chip.component.scss'],
   standalone: true,
-  host: { '[class.selected]': 'selected()' },
+  host: {
+    '[class.selected]': 'selected()',
+    '[class.selectable]': 'selectedChange.observed',
+    '[attr.role]': "selectedChange.observed ? 'button': undefined",
+    '[attr.tabindex]': "selectedChange.observed ? '0': undefined",
+    '(click)': 'emitToggledSelected()',
+    '(keydown.enter)': 'emitToggledSelected()',
+    '(keydown.space)': 'emitToggledSelected()',
+  },
 })
 export class ChipComponent {
   readonly selected = input<boolean>()
@@ -20,34 +21,7 @@ export class ChipComponent {
   @Output()
   selectedChange = new EventEmitter<boolean>()
 
-  @HostBinding('class.selectable')
-  get isSelectable() {
-    return this.selectedChange.observed
-  }
-
-  @HostBinding('attr.role')
-  get ariaRole(): string | undefined {
-    return this.isSelectable ? 'button' : undefined
-  }
-
-  @HostBinding('attr.tabindex')
-  get tabIndex(): string | undefined {
-    return this.isSelectable ? (0).toString() : undefined
-  }
-
-  @HostListener('click') protected onClick() {
-    this.emitToggledSelected()
-  }
-
-  @HostListener('keydown.space') protected onKeydownSpace() {
-    this.emitToggledSelected()
-  }
-
-  @HostListener('keydown.enter') protected onKeydownEnter() {
-    this.emitToggledSelected()
-  }
-
-  private emitToggledSelected() {
+  protected emitToggledSelected() {
     this.selectedChange.emit(!this.selected())
   }
 }
