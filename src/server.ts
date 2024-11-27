7583b0931de85407cad1609e547c773fcc185ceb
David LJ <mail@davidlj95.com>
2024-11-27 17:31:28 +0100

    refactor: use Number to cast port number to use

--- src/server.ts
+++ src/server.ts
@@ -58,8 +58,7 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-  const port = process.env['PORT'] || 4000
+  const port = Number(process.env['PORT']) || 4000
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`)
   })
