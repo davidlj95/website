aa587f61ea011c8ca24a8a9d7dd97bc439af806b
David LJ <mail@davidlj95.com>
2024-11-27 19:05:14 +0100

    refactor: replace host decorator in profile pic comp (#874)

--- src/app/resume-page/profile-section/profile-picture/profile-picture.component.ts
+++ src/app/resume-page/profile-section/profile-picture/profile-picture.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Inject } from '@angular/core'
+import { Component, Inject } from '@angular/core'
 import { METADATA } from '@/common/injection-tokens'
 import { Metadata } from '@/data/metadata'
 import { NgOptimizedImage } from '@angular/common'
@@ -8,23 +8,15 @@ import { NgOptimizedImage } from '@angular/common'
   templateUrl: './profile-picture.component.html',
   styleUrls: ['./profile-picture.component.scss'],
   imports: [NgOptimizedImage],
+  host: {
+    'aria-label': 'Profile picture',
+    '[attr.data-has-been-focused]': '_hasBeenFocused ? true : undefined',
+    '[attr.tabindex]': '_hasBeenFocused ? 0 : -1',
+  },
 })
 export class ProfilePictureComponent {
   readonly realName: string
   protected _hasBeenFocused = false
-
-  static HAS_BEEN_FOCUSED_ATTR = 'data-has-been-focused'
-
-  @HostBinding(`attr.${ProfilePictureComponent.HAS_BEEN_FOCUSED_ATTR}`)
-  get hasBeenFocused() {
-    return this._hasBeenFocused ? true : undefined
-  }
-
-  @HostBinding('attr.tabindex') get tabIndex() {
-    return this.hasBeenFocused ? 0 : -1
-  }
-
-  @HostBinding('attr.aria-label') ariaLabel = 'Profile picture'
 
   constructor(@Inject(METADATA) metadata: Metadata) {
     this.realName = metadata.realName
