b21450cb7699e990ab1bb27925d43eeb4fbf7ac5
David LJ <mail@davidlj95.com>
2024-11-28 21:15:50 +0100

    test: improve 404 page testing to avoid reading protected var

--- src/app/not-found-page/not-found-page.component.spec.ts
+++ src/app/not-found-page/not-found-page.component.spec.ts
@@ -8,20 +8,21 @@ import { Router } from '@angular/router'
 import { MockProvider } from 'ng-mocks'
 import { APP_BASE_URL } from '@/common/app-base-url'
 import { componentTestSetup } from '@/test/helpers/component-test-setup'
+import { byTestId } from '@/test/helpers/test-id'
 
 describe('NotFoundPageComponent', () => {
   let component: NotFoundPageComponent
   let fixture: ComponentFixture<NotFoundPageComponent>
-  const dummyAppUrlNoTrailingSlash = 'https://example.com'
-  const dummyRouter: Pick<Router, 'url'> = {
+  const DUMMY_APP_URL = 'https://example.com'
+  const DUMMY_ROUTER: Pick<Router, 'url'> = {
     url: '/foo',
   }
 
   beforeEach(() => {
     ;[fixture, component] = componentTestSetup(NotFoundPageComponent, {
       providers: [
-        MockProvider(APP_BASE_URL, new URL(dummyAppUrlNoTrailingSlash)),
-        MockProvider(Router, dummyRouter),
+        MockProvider(APP_BASE_URL, new URL(DUMMY_APP_URL)),
+        MockProvider(Router, DUMMY_ROUTER),
       ],
     })
     fixture.detectChanges()
@@ -31,15 +32,15 @@ describe('NotFoundPageComponent', () => {
     expect(component).toBeTruthy()
   })
 
-  describe('#currentUrlInWaybackMachine', () => {
-    it('should be the Wayback Machine URL prefix plus the current URL', () => {
-      expect(component._currentUrlInWaybackMachine).toEqual(
-        new URL(
-          WAYBACK_MACHINE_URL_PREFIX.toString() +
-            dummyAppUrlNoTrailingSlash +
-            dummyRouter.url,
-        ),
-      )
-    })
+  it('should contain a link to the Wayback Machine URL of the current page', () => {
+    const anchorElement = fixture.debugElement.query(byTestId('wayback'))
+
+    expect(anchorElement.attributes['href']).toEqual(
+      new URL(
+        WAYBACK_MACHINE_URL_PREFIX.toString() +
+          DUMMY_APP_URL +
+          DUMMY_ROUTER.url,
+      ).toString(),
+    )
   })
 })
--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -3,12 +3,13 @@ import { Lightbulb } from '@/data/material-symbols'
 import { Router } from '@angular/router'
 import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
 import { APP_BASE_URL } from '@/common/app-base-url'
+import { TestIdDirective } from '@/common/test-id.directive'
 
 @Component({
   selector: 'app-not-found-page',
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.scss'],
-  imports: [MaterialSymbolDirective],
+  imports: [MaterialSymbolDirective, TestIdDirective],
 })
 export class NotFoundPageComponent {
   protected readonly _currentUrlInWaybackMachine: URL
