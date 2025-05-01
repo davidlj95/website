3bb4a4870f160dabce7f98506f2114c659d3c83f
David LJ <mail@davidlj95.com>
2025-05-01 22:13:51 +0200

    feat: hexdump to mock canonical format
    improve whitespace treatment

--- src/app/background/background.component.spec.ts
+++ src/app/background/background.component.spec.ts
@@ -7,7 +7,7 @@ describe('BackgroundComponent', () => {
   let component: BackgroundComponent
   let fixture: ComponentFixture<BackgroundComponent>
   // 👇 SVG text size offset so that there's not empty space between repetitions
-  const HEIGHT_OFFSET = -3
+  const HEIGHT_OFFSET = -5
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,18 +25,29 @@ describe('BackgroundComponent', () => {
 
   it("should apply the SVG's text size to the SVG's pattern size", () => {
     const svgPattern = fixture.debugElement.query(By.css('svg pattern'))
-    const svgText = svgPattern.query(By.css('text'))
+
+    const flooredString = (x: number) => Math.floor(x).toString()
+
+    // 👇 After applying `white-space: pre`, the `text` takes larger width than all of `tspan`
+    //    Not sure why
+    const svgTspan = svgPattern.query(By.css('tspan'))
+    const tSpanWidth = (
+      svgTspan.nativeElement as Element
+    ).getBoundingClientRect().width
+
+    expect(tSpanWidth).withContext('tspan width heuristic').toBeGreaterThan(100)
 
     expect(svgPattern.attributes['width'])
       .withContext('width')
-      .toEqual((svgText.nativeElement as Element).clientWidth.toString())
+      .toEqual(flooredString(tSpanWidth))
+
+    const svgText = svgPattern.query(By.css('text'))
+    const textHeight = (svgText.nativeElement as Element).clientHeight
+
+    expect(textHeight).withContext('text height heuristic').toBeGreaterThan(100)
 
     expect(svgPattern.attributes['height'])
       .withContext('height')
-      .toEqual(
-        (
-          (svgText.nativeElement as Element).clientHeight + HEIGHT_OFFSET
-        ).toString(),
-      )
+      .toEqual(flooredString(textHeight + HEIGHT_OFFSET))
   })
 })
--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -6,35 +6,53 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
   templateUrl: './background.component.html',
   styleUrl: './background.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  preserveWhitespaces: true,
 })
 export class BackgroundComponent {
   protected readonly _genesisBlock = hexdump(atob(GENESIS_BLOCK_BASE_64))
   //👇 Could be calculated on the client side.
   //   However, this component is part of the largest contentful paint (LCP)
   //   So calculating it in advance. This way, there are no changes when hydrating
-  protected readonly _textSize = { width: 720, height: 290 }
+  protected readonly _textSize = { width: 777, height: 288 }
 }
 
-// https://gist.github.com/igorgatis/d294fe714a4f523ac3a3
-// With few tweaks as block size is fixed to 16
-function hexdump(buffer: string) {
+/**
+ * Source: https://gist.github.com/igorgatis/d294fe714a4f523ac3a3
+ *
+ * Adapted for TypeScript. With a few tweaks to mock `hexdump -C` output:
+ *  - Fixed block size of 16
+ *  - Address takes 8 chars
+ *  - Extra space every 8 code blocks
+ *  - Add '|' to chars block
+ *  - Do not print \x7F - \x9F chars (most can't be seen properly)
+ */
+const hexdump = (buffer: string) => {
   const blockSize = 16
   const lines: string[] = []
   const hex = '0123456789ABCDEF'
+  const SPACE = ' '
   for (let b = 0; b < buffer.length; b += blockSize) {
     const block = buffer.slice(b, Math.min(b + blockSize, buffer.length))
     const addrSize = 8
     const addr = ('0'.repeat(addrSize) + b.toString(16)).slice(-addrSize)
-    const codes = block
-      .split('')
-      .map(function (ch) {
-        const code = ch.charCodeAt(0)
-        return ' ' + hex[(0xf0 & code) >> 4] + hex[0x0f & code]
-      })
-      .join('')
-    // eslint-disable-next-line no-control-regex
-    const chars = block.replace(/[\x00-\x1F\x20]/g, '.')
-    lines.push(addr + ' ' + codes + '  |' + chars + '|')
+    const codes =
+      block
+        .split('')
+        .map((ch, index) => {
+          const code = ch.charCodeAt(0)
+          const spaces = index % 8 === 0 ? 2 : 1
+          return (
+            SPACE.repeat(spaces) + hex[(0xf0 & code) >> 4] + hex[0x0f & code]
+          )
+        })
+        .join('') + SPACE.repeat(3).repeat(blockSize - block.length)
+    const chars =
+      block.replace(
+        // eslint-disable-next-line no-control-regex
+        /[\x00-\x1F\x20\x7F-\x9F]/g,
+        '.',
+      ) + SPACE.repeat(blockSize - block.length)
+    lines.push(addr + ' ' + codes + '  |' + chars + '|' + '  ')
   }
   return lines.join('\n')
 }
