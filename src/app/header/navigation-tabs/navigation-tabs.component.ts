2fe715760348021663f2321c168990146d00fb7e
David LJ <mail@davidlj95.com>
2025-05-02 03:33:07 +0200

    refactor: use nav as host element for navigation tabs

--- src/app/header/navigation-tabs/navigation-tabs.component.ts
+++ src/app/header/navigation-tabs/navigation-tabs.component.ts
@@ -4,7 +4,8 @@ import { TabsComponent } from '../tabs/tabs.component'
 import { RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
-  selector: 'app-navigation-tabs',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'nav[appNavigationTabs]',
   imports: [TabsComponent, TabComponent, RouterLink, RouterLinkActive],
   templateUrl: './navigation-tabs.component.html',
   styleUrl: './navigation-tabs.component.scss',
