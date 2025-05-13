df79b1c8be6941d6a5e40668f5beff4c67d10657
David LJ <mail@davidlj95.com>
2025-05-13 15:58:08 +0200

    refactor: semantic date range (#1137)

--- src/app/resume-page/date-range/date-range.component.ts
+++ src/app/resume-page/date-range/date-range.component.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core'
 import { DateRange } from './date-range'
-import { DatePipe } from '@angular/common'
+import { DatePipe, NgTemplateOutlet } from '@angular/common'
 
 @Component({
   selector: 'app-date-range',
   templateUrl: './date-range.component.html',
-  imports: [DatePipe],
+  imports: [DatePipe, NgTemplateOutlet],
 })
 export class DateRangeComponent {
   readonly range = input.required<DateRange>()
+  protected readonly _now = new Date()
 }
