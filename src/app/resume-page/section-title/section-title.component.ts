a97ccf814da4069c17c20908075257977f6103dd
David LJ <mail@davidlj95.com>
2025-05-19 01:24:37 +0200

    refactor: move toolbar background and color to toolbar comp

--- src/app/resume-page/section-title/section-title.component.ts
+++ src/app/resume-page/section-title/section-title.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
+import { ToolbarComponent } from '../../header/toolbar/toolbar.component'
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: '[appSectionTitle]',
-  template: '<ng-content></ng-content>',
+  template: '<app-toolbar><ng-content></ng-content></app-toolbar>',
   styleUrls: ['./section-title.component.scss'],
+  imports: [ToolbarComponent],
 })
 export class SectionTitleComponent {}
