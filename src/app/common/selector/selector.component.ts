88cb365c04338f8c20068d3132ba0baed52c003d
David LJ <mail@davidlj95.com>
2025-05-22 18:17:01 +0200

    fix: add names to inputs

--- src/app/common/selector/selector.component.ts
+++ src/app/common/selector/selector.component.ts
@@ -14,6 +14,7 @@ import {
 })
 export class SelectorComponent<Value extends string = string> {
   readonly label = input.required<string>()
+  readonly name = input<string>()
   readonly options = input.required<readonly SelectorOption<Value>[]>()
   readonly selected = input<Value>()
   readonly selectedChange = output<Value>()
