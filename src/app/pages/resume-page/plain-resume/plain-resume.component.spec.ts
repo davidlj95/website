b58b35b9635868d5dfd07f787a397f50ef9de84b
David LJ <mail@davidlj95.com>
2025-05-22 18:23:59 +0200

    chore: fix unit test plain resume

--- src/app/pages/resume-page/plain-resume/plain-resume.component.spec.ts
+++ src/app/pages/resume-page/plain-resume/plain-resume.component.spec.ts
@@ -1,18 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture } from '@angular/core/testing'
 
 import { PlainResumeComponent } from './plain-resume.component'
+import { MockComponents, MockDirectives, MockPipes } from 'ng-mocks'
+import { NgIconComponent } from '@ng-icons/core'
+import { componentTestSetup } from '@/test/helpers/component-test-setup'
+import { LinkComponent } from '../link/link.component'
+import { DateRangeComponent } from '../date-range/date-range.component'
+import { ContentPageComponent } from '@/common/content-page/content-page.component'
+import { MdLinksPipe } from '../md-links.pipe'
+import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
+import { TechnologyComponent } from '../technology/technology.component'
 
 describe('PlainResumeComponent', () => {
   let component: PlainResumeComponent
   let fixture: ComponentFixture<PlainResumeComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PlainResumeComponent],
-    }).compileComponents()
-
-    fixture = TestBed.createComponent(PlainResumeComponent)
-    component = fixture.componentInstance
+  beforeEach(() => {
+    ;[fixture, component] = componentTestSetup(PlainResumeComponent, {
+      imports: [
+        MockComponents(
+          LinkComponent,
+          DateRangeComponent,
+          ContentPageComponent,
+          NgIconComponent,
+          TechnologyComponent,
+        ),
+        MockDirectives(MaterialSymbolDirective),
+        MockPipes(MdLinksPipe),
+      ],
+    })
     fixture.detectChanges()
   })
 
