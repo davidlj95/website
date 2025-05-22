ad6caec83a0e61001ad43b2afbd4295b0223378c
David LJ <mail@davidlj95.com>
2025-05-22 12:18:22 +0200

    feat: add button to download resume as PDF (#1186)
    * feat: add button to download resume as PDF
    
    * test: add missing ngicon comp

--- src/app/common/toolbar-button/toolbar-button.component.spec.ts
+++ src/app/common/toolbar-button/toolbar-button.component.spec.ts
@@ -1,20 +1,6 @@
 import { ToolbarButtonComponent } from './toolbar-button.component'
-import { componentTestSetup } from '@/test/helpers/component-test-setup'
-import { textContent } from '@/test/helpers/text-content'
-import { setFixtureInputsAndDetectChanges } from '@/test/helpers/set-fixture-inputs'
+import { shouldProjectContent } from '@/test/helpers/component-testers'
 
 describe('ToolbarButtonComponent', () => {
-  it('should contain icon', () => {
-    const [fixture] = makeSut()
-    fixture.detectChanges()
-
-    expect(textContent(fixture.debugElement)).toBe(DUMMY_ICON)
-  })
+  shouldProjectContent(ToolbarButtonComponent)
 })
-
-const DUMMY_ICON = 'icon'
-const makeSut = () => {
-  const [fixture, component] = componentTestSetup(ToolbarButtonComponent)
-  setFixtureInputsAndDetectChanges(fixture, { icon: DUMMY_ICON })
-  return [fixture, component] as const
-}
--- src/app/common/toolbar-button/toolbar-button.component.ts
+++ src/app/common/toolbar-button/toolbar-button.component.ts
@@ -1,16 +1,15 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'button[appToolbarButton]',
-  templateUrl: './toolbar-button.component.html',
+  selector: 'button[appToolbarButton],a[appToolbarButton]',
+  template: '<ng-content></ng-content>',
   styleUrl: './toolbar-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   hostDirectives: [MaterialSymbolDirective],
 })
 export class ToolbarButtonComponent {
-  readonly icon = input.required<string>()
   // https://github.com/angular/components/blob/18.0.5/src/material/button/button-base.ts#L257-L266
   // https://github.com/angular/components/blob/18.0.5/src/material/button/button-base.ts#L257-L266
 }
