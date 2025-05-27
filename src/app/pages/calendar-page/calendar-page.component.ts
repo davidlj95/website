f5f0d40008d726cb3a2c469d1be3028a607ccf5c
David LJ <mail@davidlj95.com>
2025-05-27 23:58:05 +0200

    feat: general design improvements

--- src/app/pages/calendar-page/calendar-page.component.ts
+++ src/app/pages/calendar-page/calendar-page.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core'
+import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
+import { Event } from '@/data/material-symbols'
 
 @Component({
   templateUrl: './calendar-page.component.html',
   styleUrl: './calendar-page.component.scss',
+  imports: [MaterialSymbolDirective],
 })
-export class CalendarPageComponent {}
+export class CalendarPageComponent {
+  protected readonly _materialSymbols = { Event }
+}
--- src/app/pages/gifts-page/gifts-page.component.ts
+++ src/app/pages/gifts-page/gifts-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core'
 import { NgOptimizedImage } from '@angular/common'
 import { ContentPageComponent } from '@/common/content-page/content-page.component'
+import { CardGiftcard } from '@/data/material-symbols'
+import { MaterialSymbolDirective } from '@/common/material-symbol.directive'
 
 @Component({
-  imports: [NgOptimizedImage, ContentPageComponent],
+  imports: [NgOptimizedImage, ContentPageComponent, MaterialSymbolDirective],
   templateUrl: './gifts-page.component.html',
 })
-export class GiftsPageComponent {}
+export class GiftsPageComponent {
+  protected readonly _materialSymbols = { CardGiftcard }
+}
